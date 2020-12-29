91f66e5bdabd73b92b56820757732c92b2793cf9
Charles <charlesangeloplai@gmail.com>
2020-12-29 18:25:52 -0500

    commit tree 99ff3d641e7961edd973064c0289f8da867033e7

---
+++ models/piglatinizer.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class PigLatinizer
+
+    def piglatinize(str)
+        str.split(" ").map {|word| piglatinize_word(word)}.join(" ")
+    end
+    
+    def vowel?(char)
+        char.match(/[aAeEiIoOuU]/)
+    end
+    
+    def piglatinize_word(word)
+        if vowel?(word[0])
+            word + "way"
+        else
+            vowel_index = word.index(/[aAeEiIoOuU]/)
+            consonants = word.slice(0..vowel_index-1)
+            word_leftover = word.slice(vowel_index..word.length)
+            word_leftover + consonants + "ay"
+        end
+    end
+end
